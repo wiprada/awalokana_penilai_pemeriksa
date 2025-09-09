0aab2f784e70d529ae1dfec7a7e7c35951e566ed
wiprada <igede.pasupati@bpk.go.id>
2025-09-09 18:44:28 +0800

    update package yang dibutuhkan

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App-old.vue'
-import router from './router/index-old'
+import App from './App.vue'
+import router from './router/index'
 
 createApp(App).use(router).mount('#app')
